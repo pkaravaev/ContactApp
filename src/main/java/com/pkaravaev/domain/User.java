f06162588d41227fe2aa7ec4b4637b3b66cbbea9
pavloid <karavaev.pavel.sergeevich@gmail.com>
2018-04-28 20:44:05 +0300

    add domain

---
+++ src/main/java/com/pkaravaev/domain/User.java
@@ -0,0 +1,97 @@
+package com.pkaravaev.domain;
+
+public class User {
+
+    private Integer userid;
+
+    private String name;
+
+    private String phone;
+
+    private String email;
+
+    private String addfess;
+
+    private String loginname;
+
+    private String password;
+
+    private int role;
+
+    public User() {
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddfess() {
+        return addfess;
+    }
+
+    public void setAddfess(String addfess) {
+        this.addfess = addfess;
+    }
+
+    public String getLoginname() {
+        return loginname;
+    }
+
+    public void setLoginname(String loginname) {
+        this.loginname = loginname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public int getLoginstatus() {
+        return loginstatus;
+    }
+
+    public void setLoginstatus(int loginstatus) {
+        this.loginstatus = loginstatus;
+    }
+
+    private int loginstatus;
+}
