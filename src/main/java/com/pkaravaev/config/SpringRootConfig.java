10c7eca34c19a196b0ab8537615f23f9fcb97020
pavloid <karavaev.pavel.sergeevich@gmail.com>
2018-04-28 21:16:15 +0300

    add test DataSource

--- src/main/java/com/pkaravaev/config/SpringRootConfig.java
+++ src/main/java/com/pkaravaev/config/SpringRootConfig.java
@@ -1,6 +1,8 @@
 package com.pkaravaev.config;
 
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,5 +10,23 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "com.pkaravaev" )
 public class SpringRootConfig   {
 
+
     //TODO: Services , DAO, DATASOURCe, EMAIL SENDER or some other Buisness layer Beans
+
+    @Bean
+    public BasicDataSource getDataSource(){
+
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/capp_db");
+        ds.setUsername("root");
+        ds.setPassword("root");
+        ds.setMaxTotal(2);
+        ds.setInitialSize(1);
+        ds.setTestOnBorrow(true);
+        ds.setValidationQuery("SELECT 1");
+        ds.setDefaultAutoCommit(true);
+
+        return ds;
+    }
 }
---
+++ src/test/java/com/pkaravaev/TestDataSource.java
@@ -0,0 +1,28 @@
+package com.pkaravaev;
+
+import com.pkaravaev.config.SpringRootConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+public class TestDataSource {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringRootConfig.class);
+
+//        DataSource dataSource = context.getBean(DataSource.class);
+//
+//        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+//
+//        String sql = "INSERT INTO user  ('name', 'phone', 'email', 'address', 'loginName', 'password') VALUES (?,?,?,?,?,?)";
+//
+//        Object[] param = new Object[]{"Pavel", "9999999", " mail@mail.ru", "Moscow", "pavel1990", "q1w2e3r4t5"};
+//
+//        jdbcTemplate.update(sql, param);
+//
+//        System.out.println("-----------SQL executed -----------");
+    }
+}
